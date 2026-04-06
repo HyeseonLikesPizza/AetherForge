ba8959a5e61c35746d9a88cf90766ca2b0484c91
HyeseonLikesPizza <1725f0701@gmail.com>
2026-04-06 21:31:54 +0900

    Initial commit

---
+++ Source/AetherForge/AetherForge.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AetherForge.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AetherForge, "AetherForge" );
