67903eee482801953f8f6df745d6409bca55832c
HyeseonLikesPizza <1725f0701@gmail.com>
2026-04-07 22:15:01 +0900

    chore: GAS 및 멀티플레이 모듈 의존성 추가
    - .uproject에 GameplayAbilities, EnhancedInput, OnlineSubsystem 플러그인 활성화
    - Build.cs에 GameplayAbilities, GameplayTags, GameplayTasks, NetCore, UMG 모듈 추가

---
+++ Source/AetherForge/Private/Character/AFPlayerCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/AFPlayerCharacter.h"
+
+
+// Sets default values
+AAFPlayerCharacter::AAFPlayerCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAFPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAFPlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AAFPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
